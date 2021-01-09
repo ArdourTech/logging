b0a924322ee9bc454cb7f267faf1f70e5400f5f4
Alexander Scott <xander@axrs.io>
2021-01-09 20:38:28 +1000

    Init

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject tech.ardour/logging "0.0.1"
+  :description "Ardour Tech Logging Tool"
+  :url "https://github.com/ArdourTech/logging"
+  :license {:name         "Eclipse Public License - v 1.0"
+            :url          "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments     "same as Clojure"}
+  :dependencies [[com.taoensso/timbre "5.1.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
+                                  [org.clojure/clojurescript "1.10.597"]]}}
+  :source-paths ["src"]
+  :test-paths ["test"])
